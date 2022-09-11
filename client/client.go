ae77a84a166575153cd97b715b8dd8bd47a89549
Shahid Ikram <shahidikram0701@Shahids-MacBook-Pro.local>
2022-09-10 22:27:57 -0500

    upgraded client to handle coordinator failure

--- client/client.go
+++ client/client.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"time"
 
 	pb "cs425/mp1/proto/coordinator_proto"
@@ -14,33 +15,48 @@ import (
 )
 
 const (
-	defaultQuery         = "shahid"
-	defaultCoordinatorIp = "172.22.156.122:50051"
+	defaultQuery = "shahid"
 )
 
 var (
-	coordinatorIp = flag.String("coordinatorip", defaultCoordinatorIp, "Coordinator IP")
-	query         = flag.String("query", defaultQuery, "Query to search for")
+	devmode      = flag.Bool("devmode", false, "Develop locally?")
+	query        = flag.String("query", defaultQuery, "Query to search for")
+	coordinators = []string{"172.22.156.122:50051", "172.22.158.122:50051", "172.22.94.122:50051", "172.22.156.123:50051", "172.22.158.123:50051", "172.22.94.123:50051", "172.22.156.124:50051", "172.22.158.124:50051", "172.22.94.124:50051", "172.22.156.125:50051"}
 )
 
 func main() {
 	flag.Parse()
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(*coordinatorIp, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatalf("Failed to establish connection with the coordinator")
-	}
-	defer conn.Close()
-	c := pb.NewCoordinatorClient(conn)
+	var conn *grpc.ClientConn
+	var err error
+	var coordinatorIp string
+	for {
+		if *devmode {
+			coordinatorIp = "localhost:50051"
+		} else {
+			coordinatorIndex := rand.Intn(10)
+			coordinatorIp = coordinators[coordinatorIndex]
+		}
 
-	// Contact the server and print out its response.
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	defer cancel()
-	r, err := c.QueryLogs(ctx, &pb.QueryRequest{Query: *query, IsTest: false})
-	if err != nil {
-		log.Fatalf("Failed to query logs: %v", err)
+		log.Printf("Coordinator: %v", coordinatorIp)
+		conn, err = grpc.Dial(coordinatorIp, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			log.Printf("Failed to establish connection with the coordinator....Retrying")
+		}
+
+		defer conn.Close()
+		c := pb.NewCoordinatorClient(conn)
+
+		// Contact the server and print out its response.
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		defer cancel()
+		r, err := c.QueryLogs(ctx, &pb.QueryRequest{Query: *query, IsTest: false})
+		if err != nil {
+			log.Printf("Failed to establish connection with the coordinator....Retrying")
+		} else {
+			log.Printf("Successfully fetched logs")
+			fmt.Printf(r.GetLogs())
+			log.Printf("Total Matches: %v", r.GetTotalMatches())
+			break
+		}
 	}
-	log.Printf("Successfully fetched logs")
-	fmt.Printf(r.GetLogs())
-	log.Printf("Total Matches: %v", r.GetTotalMatches())
 }
